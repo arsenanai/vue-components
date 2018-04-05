e66ab7bb66428bc3c27b35283433c572db69481a
Arsen Anay <arsenanai@gmail.com>
2018-04-05 11:12:29 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <app-server-titles></app-server-titles> 
+            <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Header.vue';
+    import Footer from './components/Footer.vue';
+    import ServerTitles from './components/ServerTitles.vue';
+    import ServerDetails from './components/ServerDetails.vue';
+    export default {
+        components: {
+            'app-header': Header,
+            'app-server-titles': ServerTitles,
+            'app-server-details': ServerDetails,
+            'app-footer': Footer
+        }
+    }
+</script>
+
+<style>
+</style>
